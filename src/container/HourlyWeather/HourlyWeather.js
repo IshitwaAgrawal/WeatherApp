537ef155db55457ce885dee871a2836279ebbfca
Pulkit9068 <pulkit94110@gmail.com>
2020-12-01 13:50:05 +0530

    added carousel

--- src/container/HourlyWeather/HourlyWeather.js
+++ src/container/HourlyWeather/HourlyWeather.js
@@ -1,19 +1,92 @@
 import React from 'react'
 import WeatherComponent from '../../components/WeatherComponent/WeatherComponent';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function HourlyWeather() {
+    var settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 8,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    };
+
     return (
         <div className="dailyweather">
             <h1>Hourly</h1>
-            <div className="dailyweather__items">
+            {/* <div className="dailyweather__items"> */}
+                <Slider {...settings}>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                    <div>
+                        <WeatherComponent hourly />
+                    </div>
+                </Slider>
+
+                {/* <WeatherComponent hourly />
                 <WeatherComponent hourly />
                 <WeatherComponent hourly />
                 <WeatherComponent hourly />
                 <WeatherComponent hourly />
                 <WeatherComponent hourly />
-                <WeatherComponent hourly />
-                <WeatherComponent hourly />
-            </div>
+                <WeatherComponent hourly /> */}
+            {/* </div> */}
         </div>
     )
 }

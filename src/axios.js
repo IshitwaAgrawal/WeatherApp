797a3230fd13ad1ba4c1dfaf59e1244e4f425028
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-08-13 08:27:56 +0000

    changed api address

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'https://weatherapp-server.herokuapp.com/api/'
+    //baseURL:'https://weatherapp-server.herokuapp.com/api/'
+      baseURL:'http://172.105.54.25:1357/api/'
 });
 
 export default instance;

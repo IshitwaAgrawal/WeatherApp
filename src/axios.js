3e9748ec1b29722da085048450c51f5b895b65e1
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-10-01 18:37:20 +0000

    changed docker config

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL:'https://weatherapp-server.herokuapp.com/api/'
-      baseURL:'http://172.105.54.25:1357/api/'
+      baseURL:'http://ishitw.me:1357/api/'
 });
 
 export default instance;

0419065099708e58537b55ba1639ed9dfa85d04d
Ishitwa Agrawal <47420924+ishitwa2000@users.noreply.github.com>
2023-07-29 12:29:45 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL:'https://weatherapp-server.herokuapp.com/api/'
-      baseURL:'http://45.79.217.63:1357/api/'
+      baseURL:'http://45.79.217.63:1357/api/',
+      withCredentials: false,
 });
 
 export default instance;
